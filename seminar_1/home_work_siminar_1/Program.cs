f41ed28850fe758a8d0be0e33536d40d517e895b
Salavat <s.faskhutdinov@inbox.ru>
2022-08-18 14:42:22 +0300

    Add new folder home_work_seminar_1

---
+++ seminar_1/home_work_siminar_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+
+Console.Write("Введите 2 числа: ");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int min = num1;
+int max = num1;
+
+if(min < num2)
+{
+    min = num2;
+} 
+else
+{
+max = num2;
+}
+
+Console.WriteLine(" max = " + max);
+Console.WriteLine(" min = " + min);
