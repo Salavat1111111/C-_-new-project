7c9ca4968fbaf554e5ec47a3bc0b54f0b03ae489
Salavat <s.faskhutdinov@inbox.ru>
2022-08-18 13:46:54 +0300

    Add new folder seminar_1

---
+++ seminar_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+// 1. На вход число на ывход его квадрат.
+
+Console.Write("Ввведите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sqrt = Convert.ToInt32(Math.Pow(num, 2));
+
+Console.WriteLine("Число в квадрате " + sqrt);
+
+
